689275a97b0e22435cc641619b65880d88a49064
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2025-05-14 15:22:38 +0530

    Adding Job Search Part

--- index.js
+++ index.js
@@ -12,11 +12,18 @@ dotenv.config();
 
 const app = express();
 const upload = multer({ dest: 'uploads/resumes/' });
+const allowedOrigins = ['http://localhost:5173', 'https://hunt360.vercel.app', 'https://hunt360.onrender.com'];
 
 app.use(cors({
-    origin: '*'
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, origin);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 }));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
