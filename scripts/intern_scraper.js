5af1520b6a6b5053cac11ff8a8c8139ad6c0ada3
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2025-05-19 13:36:32 +0530

    Updating Packages

--- scripts/intern_scraper.js
+++ scripts/intern_scraper.js
@@ -7,7 +7,6 @@ import chrome from 'selenium-webdriver/chrome.js';
 import XLSX from 'xlsx';
 import { fileURLToPath } from 'url';
 
-import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
